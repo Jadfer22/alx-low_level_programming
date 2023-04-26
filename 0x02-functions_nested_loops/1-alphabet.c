a11eaf09a4eb14a68c21e276fb5687a521e617b7
Jadfer22 <godgiftjoshua22@gmail.com>
2023-04-26 12:41:22 -0700

    putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
 * print_alphabet - print all alpha in lowercase
+* Return: void
 */
 void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	{
+        _putchar(letter);
+	}
 	_putchar('\n');
 }
