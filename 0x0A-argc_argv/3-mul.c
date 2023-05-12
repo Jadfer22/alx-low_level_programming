df02be10a2e3dba1c7b412f5b5d4e60069e54577
Jadfer22 <godgiftjoshua22@gmail.com>
2023-05-12 08:47:50 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,22 +3,20 @@
 #include <stdio.h>
 
 /**
- * _atoi - program prints the multiplications of two integers
+ * main - program prints the multiplications of two integers
  * @argc: counts of arguments
  * @argv: arrays of arguments
  * Return: 0 if true, 1 if false
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
 
 	if (argc != 3)
 	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		printf("%d\n", a * b);
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-	printf("Error\n");
-	return (1);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
+
